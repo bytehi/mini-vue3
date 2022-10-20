0bc0af26963e2d09a0657ace7c0b20e9b2c41d2f
zhangdongli <zhangdl@dachentech.com.cn>
2022-10-20 12:35:21 +0800

    feat: 添加reactivity响应式模块

---
+++ packages/reactivity/src/reactive.ts
@@ -0,0 +1,18 @@
+import { mutableHandlers } from "./baseHandlers"
+
+export const reactiveMap = new WeakMap()
+
+
+export function reactive(target) {
+  createReactiveObject(target, reactiveMap, mutableHandlers)
+}
+
+function createReactiveObject(target, proxyMap, baseHandlers) {
+  const existingProxy = proxyMap.get(target)
+  if (existingProxy) {
+    return existingProxy
+  }
+  const proxy = new Proxy(target, baseHandlers)
+  proxyMap.set(target, proxy)
+  return proxy
+}
