aa007b5caa2216bb0c9cd64ce2394c4f4ef3cd6f
zhangdongli <zhangdl@dachentech.com.cn>
2022-10-20 15:52:40 +0800

    添加get、set函数

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,12 +1,31 @@
+import { reactive, ReactiveFlags, reactiveMap } from "./reactive"
 
 const get = createGetter()
 const set = createSetter()
 
 function createGetter() { 
+  return function get(target, key, receiver) {
+    
+    const isExistingReactiveMap = () => 
+      key === ReactiveFlags.RAW && receiver === reactiveMap.get(target)
+    
+    
+    const res = Reflect.get(target, key, receiver)
+    
+    if (isExistingReactiveMap()) {
+      return target;
+    }
 
+    return res;
+  }
 }
-function createSetter() { 
 
+function createSetter() { 
+  return function set(target, key, value, receiver) {
+    const result = Reflect.set(target, key, value, receiver)
+    // TODO 触发依赖收集
+    return result
+  }
 }
 
 export const mutableHandlers = {
